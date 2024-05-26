9b026135a512ce3d7cf37db2af563e432f4aa5e4
Vladimir Zadorozhnyy <balancon@mail.ru>
2024-05-26 17:13:26 +0300

    Homework1 task 5 is completed.

--- 5_while2.py
+++ 5_while2.py
@@ -15,13 +15,17 @@
     
 """
 
-questions_and_answers = {}
+questions_and_answers = {
+    "Как дела?": "Хорошо!", 
+    "Что делаешь?": "Программирую", 
+    "Как настроение?": "Отлично!", 
+    "Как погода?": "Прохладно"}
 
 def ask_user(answers_dict):
-    """
-    Замените pass на ваш код
-    """
-    pass
+
+    print("Введите вопрос:")
+    while True:
+        print(questions_and_answers.get(input()))
     
 if __name__ == "__main__":
     ask_user(questions_and_answers)
